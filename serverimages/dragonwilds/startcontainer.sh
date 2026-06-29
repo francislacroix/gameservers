f39cab550c170402c074f4885dc1fc2951f3a37e
Francis Lacroix <francis.lacroix@outlook.com>
2026-06-28 21:59:53 -0400

    Added container name

--- serverimages/dragonwilds/startcontainer.sh
+++ serverimages/dragonwilds/startcontainer.sh
@@ -2,4 +2,4 @@
 REPO_NAME=$1
 
 docker pull $REPO_NAME/dragonwilds:latest
-docker run -d -p 7777:7777/udp -p 7778:7778/udp $REPO_NAME/dragonwilds:latest --restart unless-stopped
+docker run -d --name dragonwilds -p 7777:7777/udp -p 7778:7778/udp $REPO_NAME/dragonwilds:latest --restart unless-stopped
