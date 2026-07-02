77f6b7d32d66eca13a193d472a9eb36670710e9b
Francis Lacroix <francis.lacroix@outlook.com>
2026-07-02 14:10:13 -0400

    Updated download script

--- scripts/downloadserver.sh
+++ scripts/downloadserver.sh
@@ -4,20 +4,22 @@
 servertype=""
 
 # Get the named parameter
-while getopts ":t" opt; do
+while getopts ":t:" opt; do
     case $opt in
         t) servertype="$OPTARG" ;;
+        :) echo "Error: Option -$OPTARG requires an argument." >&2; exit 1 ;;
+        \?) echo "Error: Invalid option -$OPTARG" >&2; exit 1 ;;
     esac
 done
 
 # Validate required parameters
 if [[ -z "$servertype" ]]; then
     echo "Error: The -t <servertype> parameter is required." >&2
-    usage
+    exit 1
 fi
 
 curl -O https://raw.githubusercontent.com/francislacroix/gameservers/refs/heads/main/servers/$servertype/.env.sample
 curl -O https://raw.githubusercontent.com/francislacroix/gameservers/refs/heads/main/servers/$servertype/Dockerfile
 curl -O https://raw.githubusercontent.com/francislacroix/gameservers/refs/heads/main/servers/$servertype/compose.yaml
 
-mv .env.sample .env
+mv ./.env.sample ./.env
